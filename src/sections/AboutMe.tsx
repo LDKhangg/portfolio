be93a7707e1054787c7ed1470354fe5c358f063a
LDKhangg <lekhangmc12@gmail.com>
2026-08-23 21:09:27 +0700

    fix: raise about section stop

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -3,7 +3,7 @@ import { Section, SectionAnchor, SectionHeading } from "@/components/layout";
 import { content } from "@/content";
 
 const AboutAnchor = styled(SectionAnchor)`
-  scroll-margin-top: 96px;
+  scroll-margin-top: 72px;
 `;
 
 const Wrap = styled.div`
